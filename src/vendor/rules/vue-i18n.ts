effdf4dd54bdf62eff0f005b7f58b68d78afcf90
Konstantinos Paparas <kelsos86@gmail.com>
2024-03-12 11:15:18 +0100

    chore: update dependencies

--- src/vendor/rules/vue-i18n.ts
+++ src/vendor/rules/vue-i18n.ts
@@ -1,32 +1,32 @@
 import type { RuleConfig } from '@antfu/eslint-define-config';
 
 export interface VueI18nRules {
-  'key-format-style': RuleConfig<['camelCase' | 'kebab-case' | 'snake_case', { 'allowArray'?: boolean; 'splitByDots'?: boolean }]>;
+  'key-format-style': RuleConfig<['camelCase' | 'kebab-case' | 'snake_case', { allowArray?: boolean; splitByDots?: boolean }]>;
   'no-deprecated-i18n-component': RuleConfig<[]>;
   'no-deprecated-i18n-place-attr': RuleConfig<[]>;
   'no-deprecated-i18n-places-prop': RuleConfig<[]>;
-  'no-duplicate-keys-in-locale': RuleConfig<[{ 'ignoreI18nBlock'?: boolean }]>;
+  'no-duplicate-keys-in-locale': RuleConfig<[{ ignoreI18nBlock?: boolean }]>;
   'no-dynamic-keys': RuleConfig<[]>;
   'no-html-messages': RuleConfig<[]>;
   'no-i18n-t-path-prop': RuleConfig<[]>;
   'no-missing-keys': RuleConfig<[]>;
-  'no-missing-keys-in-other-locales': RuleConfig<[{ 'ignoreLocales'?: string[] }]>;
+  'no-missing-keys-in-other-locales': RuleConfig<[{ ignoreLocales?: string[] }]>;
   'no-raw-text': RuleConfig<[{
-    'attributes'?: Record<string, string[]>;
-    'ignoreNodes'?: string[];
-    'ignorePattern'?: string;
-    'ignoreText'?: string[];
+    attributes?: Record<string, string[]>;
+    ignoreNodes?: string[];
+    ignorePattern?: string;
+    ignoreText?: string[];
   }]>;
   'no-unknown-locale': RuleConfig<[{
-    'locales'?: string[];
-    'disableRFC5646'?: boolean;
+    locales?: string[];
+    disableRFC5646?: boolean;
   }]>;
   'no-unused-keys': RuleConfig<[
     {
-      'src'?: string;
-      'extensions'?: string[];
-      'ignores'?: string[];
-      'enableFix'?: boolean;
+      src?: string;
+      extensions?: string[];
+      ignores?: string[];
+      enableFix?: boolean;
     },
   ]>;
   'no-v-html': RuleConfig<[]>;
